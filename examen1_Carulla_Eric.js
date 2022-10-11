772316e9b9ce9278c276cfb818e8160ae011af7f
Cire9 <a21ericarsan@inspedralbes.cat>
2022-10-11 12:04:57 +0200

    Commit2

---
+++ examen1_Carulla_Eric.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const path = require('path');
+
+   // Node.js program to demonstrate the
+// fs.existsSync() method
+  
+
+
+  
+// Check if the file exists
+let DirExists = fs.existsSync('log_programa');
+console.log( DirExists, "El directori s'ha trobat a la ruta" + );
+if (!DirExists) {
+    console.log("Crean directori...")
+
+fs.mkdir(path.join(__dirname, 'log_programa'), (err) => {
+    if (err) {
+        return console.error('Ja existeix el directori');
+    }
+    console.log('El directori ha sigut creat');
+})};
